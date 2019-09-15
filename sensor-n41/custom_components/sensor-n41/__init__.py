6f38d964be49ae68e223040cc4f67ce12104eac6
haiyang.song <songhaiyang@shuwen.com>
2019-09-15 22:29:01 +0800

    rm sensor-n41

--- sensor-n41/custom_components/sensor-n41/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf8 -*-
-
-# author          :haiyang.song
-# email           :meishild@gmail.com
-# datetime        :2019-08-27
-# version         :1.0
-# python_version  :3.4.3
-# description     :
-# ==============================================================================
